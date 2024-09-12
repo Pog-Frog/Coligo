eea947643614b6c0dbdf9490923c99a1af804013
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2024-09-12 23:25:56 +0300

    Refactor routes in server.ts to use ExamRoute and AnnouncementRoute

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
 import {App} from "./app";
 import {AuthRoute} from "./routes/auth.route";
-import {TaskRoute} from "./routes/task.route";
-import {CategoryRoute} from "./routes/category.route";
+import {ExamRoute} from "./routes/exam.route";
+import {AnnouncementRoute} from "./routes/announcement.route";
 
 
-const app = new App([new AuthRoute(), new TaskRoute(), new CategoryRoute()]);
+const app = new App([new AuthRoute(), new ExamRoute(), new AnnouncementRoute()]);
 
 app.listen();
