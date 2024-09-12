2fedaeb856301483677e987b1ec721824fc00841
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2024-09-12 19:43:38 +0300

    updated the prisma schema and the db name

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -6,7 +6,7 @@ export const PORT = process.env.PORT || 8080;
 export const URL = process.env.URL || 'http://localhost';
 export const JWT_SECRET = process.env.JWT_SECRET || 'SUS';
 export const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017';
-export const DB_NAME = process.env.DB_NAME || 'todo';
+export const DB_NAME = process.env.DB_NAME || 'coligo';
 export const LOG_FORMAT = process.env.LOG_FORMAT || 'dev';
 export const ORIGIN = process.env.ORIGIN || '*';
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
